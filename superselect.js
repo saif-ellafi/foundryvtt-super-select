f3457e137e32a81a9ed2f6625b7808c70ee8b7b1
Saif Addin <saif1988@gmail.com>
2021-04-14 01:01:02 +0200

    Added setting to control default state

--- superselect.js
+++ superselect.js
@@ -6,25 +6,33 @@ class SuperSelect {
     'DrawingsLayer'
   ]
 
+  static _activateSuperMode() {
+    let mergedObjects = canvas.activeLayer.objects.children
+    const otherLayers = SuperSelect.ACTIVE_LAYERS.filter(layer => layer != canvas.activeLayer.name);
+    for (let i = 0; i < otherLayers.length; i++) {
+      const enriched = canvas.getLayer(otherLayers[i]).placeables.map(child => {
+        if (child.updateSource == undefined) {
+          child.updateSource = function () {};
+        }
+        return child;
+      });
+      mergedObjects = mergedObjects.concat(enriched);
+    };
+    canvas.activeLayer.objects.children = mergedObjects;
+  }
+
+  static _deactivateSuperMode() {
+    canvas.activeLayer.releaseAll();
+    const placeables = canvas.activeLayer.placeables;
+    const originalPlaceables = placeables.filter(child => child.layer.name == canvas.activeLayer.name);
+    canvas.activeLayer.objects.children = originalPlaceables;
+  }
+
   static _addControls(toggled) {
     if (toggled) {
-      let mergedObjects = canvas.activeLayer.objects.children
-      const otherLayers = SuperSelect.ACTIVE_LAYERS.filter(layer => layer != canvas.activeLayer.name);
-      for (let i = 0; i < otherLayers.length; i++) {
-        const enriched = canvas.getLayer(otherLayers[i]).placeables.map(child => {
-          if (child.updateSource == undefined) {
-            child.updateSource = function () {};
-          }
-          return child;
-        });
-        mergedObjects = mergedObjects.concat(enriched);
-      };
-      canvas.activeLayer.objects.children = mergedObjects;
+      SuperSelect._activateSuperMode();
     } else {
-      canvas.activeLayer.releaseAll();
-      const placeables = canvas.activeLayer.placeables;
-      const originalPlaceables = placeables.filter(child => child.layer.name == canvas.activeLayer.name);
-      canvas.activeLayer.objects.children = originalPlaceables;
+      SuperSelect._deactivateSuperMode();
     }
   }
 
@@ -36,6 +44,7 @@ class SuperSelect {
           title: "Super Select",
           icon: "fas fa-object-group",
           toggle: true,
+          active: game.settings.get("super-select", "startEnabled"),
           visible: game.user.isGM,
           onClick: SuperSelect._addControls,
           layer: control.layer,
@@ -44,9 +53,26 @@ class SuperSelect {
       }
     })
   }
-
 }
 
 Hooks.on('getSceneControlButtons', (controls) => {
-  SuperSelect._getControlButtons(controls)
+  if (game.user.isGM) {
+    SuperSelect._getControlButtons(controls)
+    if (game.settings.get("super-select", "startEnabled")) {
+      SuperSelect._activateSuperMode();
+    }
+  }
+});
+
+Hooks.once('init', () => {
+
+  game.settings.register("super-select", "startEnabled", {
+    name: "Super Select enabled by Default",
+    hint: "Whether to have Super Select toggled on by default.",
+    scope: "world",
+    config: true,
+    default: false,
+    type: Boolean
+  });
+
 });
